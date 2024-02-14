a035c1994bbe4d12f91f079a3242e38362388fce
Ttst <thibautstachnick@gmail.com>
2024-02-14 16:14:18 +0100

    add the way for getting word from request

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,49 +7,55 @@ use App\Entity\Word;
 use App\Repository\WordRepository;
 use App\Service\WordEmbeddingService;
 use Doctrine\ORM\EntityManagerInterface;
-use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(WordRepository $repository,WordEmbeddingService $service, EntityManagerInterface $manager): Response
+    public function checkWords(Request $request,
+                               WordRepository $repository,
+                               WordEmbeddingService $service,
+                               EntityManagerInterface $manager,
+    ): Response
     {
-        $searchedWord = "banane";
+        $searchedWord = "zombie";
+        $actualWord = $request->getPayload()->get("word");
 
-        foreach ($repository->findBy(["value" => $searchedWord])[0]->getEmbeddings() as $embedding) {
+        $searchedWordArray =$repository->findBy(["value" => $searchedWord])[0];
+        $actualWordArray = $repository->findBy(["value"=>$actualWord]);
+
+        foreach ($searchedWordArray->getEmbeddings() as $embedding) {
             $searchedWordEmbeddings[] = $embedding->getValue();
         }
 
-        
-        $actualWord = "poire";
-        if ($repository->findBy(["value"=>$actualWord])){
-            foreach ($repository->findBy(["value"=>$actualWord])[0]->getEmbeddings() as $embedding) {
-                $embeddings[] = $embedding->getValue();
-                $result = $service->cosine_similarity($embeddings,$searchedWordEmbeddings);
-            }
-            $response = [
-                "content"=>$result
-            ];
-            return $this->json($response,200);
-        }
-        $newWord = new Word();
-        $newWord->setValue($actualWord);
-        $content = $service->calculateEmbedding($actualWord);
-        foreach ($content["embedding"] as $embedding){
 
-          $newEmbedding = new Embedding();
-          $newEmbedding->setValue($embedding);
-          $newEmbedding->setWord($newWord);
-          $manager->persist($newWord);
-          $manager->persist($newEmbedding);
+        if ($actualWordArray){
+            foreach ($actualWordArray[0]->getEmbeddings() as $embedding) {
+                $embeddings[] = $embedding->getValue();
+            }
+        }else{
+            $newWord = new Word();
+            $newWord->setValue($actualWord);
+            $content = $service->calculateEmbedding($actualWord);
+            foreach ($content["embedding"] as $embedding){
+                $embeddings[] = $embedding;
+                $newEmbedding = new Embedding();
+                $newEmbedding->setValue($embedding);
+                $newEmbedding->setWord($newWord);
+                $manager->persist($newWord);
+                $manager->persist($newEmbedding);
+            }
+            $manager->flush();
         }
-        $result = "C'est tout bon";
-        $manager->flush();
+
+
+        $result = $service->cosine_similarity($embeddings,$searchedWordEmbeddings);
         $response = [
-            "content"=>$result
+            "value"=>$result
         ];
         return $this->json($response,200);
     }
