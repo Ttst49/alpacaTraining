8ae55fd6a2d86c240f98ecb84a7361e4e5faf829
Ttst <thibautstachnick@gmail.com>
2024-02-21 11:44:37 +0100

    add new entity day that define each day of game with daily changing word

---
+++ src/Entity/Day.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DayRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DayRepository::class)]
+class Day
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $day = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $word = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDay(): ?\DateTimeInterface
+    {
+        return $this->day;
+    }
+
+    public function setDay(\DateTimeInterface $day): static
+    {
+        $this->day = $day;
+
+        return $this;
+    }
+
+    public function getWord(): ?string
+    {
+        return $this->word;
+    }
+
+    public function setWord(string $word): static
+    {
+        $this->word = $word;
+
+        return $this;
+    }
+}
